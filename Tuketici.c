5917b2f9b02e19dfc32bce53b70f952347d8601e
Mohamed Nour <alkhalafmuhamade@gmail.com>
2023-08-12 02:57:32 +0300

    fixed Tuketici.c

--- Tuketici.c
+++ Tuketici.c
@@ -3,18 +3,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-extern int sayac;
 int fd[2];  // pipe için
 
 void *thread_fonksiyonu(void *arg) {
     int okunan_deger;
     int id = *((int *)arg);
 
-    if (sayac > 0) {
-        read(fd[0], &okunan_deger, sizeof(okunan_deger));
-        printf("Tüketici thread %d: %d okundu.\n", id, okunan_deger);
-        sayac--;
-    }
+    read(fd[0], &okunan_deger, sizeof(okunan_deger));
+    printf("Tüketici thread %d: %d okundu.\n", id, okunan_deger);
 
     sleep(1);
     return NULL;
@@ -34,23 +30,18 @@ int main() {
         close(fd[0]);
         int gonderilen_deger;
         while (1) {
-            if (sayac == 10) {
-                for (int i = 0; i < 10; i++) {
-                    read(STDIN_FILENO, &gonderilen_deger, sizeof(gonderilen_deger));
-                    write(fd[1], &gonderilen_deger, sizeof(gonderilen_deger));
-                }
-                sayac = 1; // Reset the sayac to 1
+            for (int i = 0; i < 10; i++) {
+                read(STDIN_FILENO, &gonderilen_deger, sizeof(gonderilen_deger));
+                write(fd[1], &gonderilen_deger, sizeof(gonderilen_deger));
             }
         }
     } else if (pid > 0) {  // Ana süreç
         close(fd[1]);
         while (1) {
-            while(sayac == 10) {
-                for (int i = 0; i < 10; i++) {
-                    thread_ids[i] = i + 1;
-                    pthread_create(&threads[i], NULL, thread_fonksiyonu, &thread_ids[i]);
-                    pthread_join(threads[i], NULL);
-                }
+            for (int i = 0; i < 10; i++) {
+                thread_ids[i] = i + 1;
+                pthread_create(&threads[i], NULL, thread_fonksiyonu, &thread_ids[i]);
+                pthread_join(threads[i], NULL);
             }
         }
     } else {
