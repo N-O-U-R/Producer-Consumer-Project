9f7e94b0142dfd7711f6e50ee14e97a08389a95c
Mohamed Nour <alkhalafmuhamade@gmail.com>
2023-08-12 01:49:01 +0300

    The code for the Producer and the Consumer

---
+++ Tuketici.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int fd[2];  // pipe için
+
+void *thread_fonksiyonu(void *arg) {
+    int okunan_deger;
+
+    read(fd[0], &okunan_deger, sizeof(okunan_deger));
+    printf("Tüketici thread %d: %d okundu.\n", (int)arg, okunan_deger);
+
+    sleep(1);
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[10];
+
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+    if (pid == 0) {  // Yavru süreç
+        close(fd[0]);
+        int gonderilen_deger;
+        while (1) {
+            for (int i = 0; i < 10; i++) {
+                read(STDIN_FILENO, &gonderilen_deger, sizeof(gonderilen_deger));
+                write(fd[1], &gonderilen_deger, sizeof(gonderilen_deger));
+            }
+        }
+    } else if (pid > 0) {  // Ana süreç
+        close(fd[1]);
+        while (1) {
+            for (int i = 0; i < 10; i++) {
+                pthread_create(&threads[i], NULL, thread_fonksiyonu, (void*)i);
+            }
+
+            for (int i = 0; i < 10; i++) {
+                pthread_join(threads[i], NULL);
+            }
+        }
+    } else {
+        perror("fork");
+        exit(1);
+    }
+
+    return 0;
+}
---
+++ Uretici.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int sayac = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int fd[2];  // pipe için
+
+void *thread_fonksiyonu(void *arg) {
+    int id = *((int *)arg);
+
+    pthread_mutex_lock(&mutex);
+    sayac = id + 1;
+    write(fd[1], &sayac, sizeof(sayac));
+    printf("Üretici thread %d: %d yazıldı.\n", id, sayac);
+    pthread_mutex_unlock(&mutex);
+    sleep(1);
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[10];
+    int thread_ids[10];
+
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+    if (pid == 0) {  // Yavru süreç
+        close(fd[1]);
+        while (1) {
+            int okunan_deger;
+            for (int i = 0; i < 10; i++) {
+                read(fd[0], &okunan_deger, sizeof(okunan_deger));
+                printf("Yavru Üretici: %d okundu.\n", okunan_deger);
+            }
+        }
+    } else if (pid > 0) {  // Ana süreç
+        close(fd[0]);
+        while (1) {
+            for (int i = 0; i < 10; i++) {
+                thread_ids[i] = i;
+                pthread_create(&threads[i], NULL, thread_fonksiyonu, &thread_ids[i]);
+            }
+
+            for (int i = 0; i < 10; i++) {
+                pthread_join(threads[i], NULL);
+            }
+        }
+    } else {
+        perror("fork");
+        exit(1);
+    }
+
+    return 0;
+}
