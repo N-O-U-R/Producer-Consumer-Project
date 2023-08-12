a0f0b415a95c92534f4129226e9da901875abe9b
Mohamed Nour <alkhalafmuhamade@gmail.com>
2023-08-12 03:47:07 +0300

    a

--- Tuketici.c
+++ Tuketici.c
@@ -6,22 +6,9 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 
-int fd[2];  // pipe için
-
 void *thread_fonksiyonu(void *arg) {
-    int okunan_deger;
-
-    sem_t *sem_uretici = sem_open("/sem_uretici", O_CREAT, 0644, 0);
-    sem_t *sem_tuketici = sem_open("/sem_tuketici", O_CREAT, 0644, 10);
-    sem_wait(sem_uretici);  // Wait for Uretici signal
-
-    read(fd[0], &okunan_deger, sizeof(okunan_deger));
+    int okunan_deger = *((int*) arg);
     printf("Tüketici thread: %d okundu.\n", okunan_deger);
-    sem_post(sem_tuketici);  // Signal to Uretici that a number is consumed
-
-    sem_close(sem_uretici);
-    sem_close(sem_tuketici);
-
     sleep(1);
     return NULL;
 }
@@ -29,16 +16,20 @@ void *thread_fonksiyonu(void *arg) {
 int main() {
     pthread_t threads[10];
 
-    if(pipe(fd) == -1) {
-        perror("pipe");
-        exit(1);
-    }
+    sem_t *sem_uretici = sem_open("/sem_uretici", O_CREAT, 0644, 0);
 
-    while(1) {
-        for(int i = 0; i < 10; i++) {
-            pthread_create(&threads[i], NULL, thread_fonksiyonu, NULL);
+    int counter = 0;
+    while (1) {
+        for (int i = 0; i < 10; i++) {
+            sem_wait(sem_uretici);
+            counter++;
+            pthread_create(&threads[i], NULL, thread_fonksiyonu, &counter);
             pthread_join(threads[i], NULL);
         }
+        counter = 0;
     }
+
+    sem_close(sem_uretici);
+    sem_unlink("/sem_uretici");
     return 0;
 }
--- Uretici.c
+++ Uretici.c
@@ -7,12 +7,10 @@
 
 int sayac = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-int fd[2];  // for data
 
 void *thread_fonksiyonu(void *arg) {
     pthread_mutex_lock(&mutex);
     sayac++;
-    write(fd[1], &sayac, sizeof(sayac));
     printf("Üretici thread %d: %d yazıldı.\n", sayac, sayac);
     pthread_mutex_unlock(&mutex);
     sleep(1);
@@ -22,28 +20,18 @@ void *thread_fonksiyonu(void *arg) {
 int main() {
     pthread_t threads[10];
 
-    if (pipe(fd) == -1) {
-        perror("pipe");
-        exit(1);
-    }
-
-    sem_t *sem_uretici = sem_open("/sem_uretici", O_CREAT, 0644, 0);  // Uretici semaphore
-    sem_t *sem_tuketici = sem_open("/sem_tuketici", O_CREAT, 0644, 10);  // Tuketici semaphore
+    sem_t *sem_uretici = sem_open("/sem_uretici", O_CREAT, 0644, 0);
 
     while (1) {
         for (int i = 0; i < 10; i++) {
             pthread_create(&threads[i], NULL, thread_fonksiyonu, NULL);
             pthread_join(threads[i], NULL);
-            sem_wait(sem_tuketici);  // Decrement the Tuketici semaphore
-        }
-
-        for (int i = 0; i < 10; i++) {
-            sem_post(sem_uretici);  // Signal to Tuketici that a number is ready
+            sem_post(sem_uretici);
         }
         sayac = 0;
     }
 
     sem_close(sem_uretici);
-    sem_close(sem_tuketici);
+    sem_unlink("/sem_uretici");
     return 0;
 }
