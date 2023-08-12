d97a2bb49cb2af690bbcf41079a3380f19b69917
Mohamed Nour <alkhalafmuhamade@gmail.com>
2023-08-12 03:14:01 +0300

    added some features

--- Tuketici.c
+++ Tuketici.c
@@ -3,7 +3,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int fd[2];  // pipe için
+int fd[2];  // for data
+int sync_pipe[2];  // for synchronization
 
 void *thread_fonksiyonu(void *arg) {
     int okunan_deger;
@@ -18,7 +19,7 @@ void *thread_fonksiyonu(void *arg) {
 int main() {
     pthread_t threads[10];
 
-    if (pipe(fd) == -1) {
+    if (pipe(fd) == -1 || pipe(sync_pipe) == -1) {
         perror("pipe");
         exit(1);
     }
@@ -26,12 +27,18 @@ int main() {
     pid_t pid = fork();
     if (pid == 0) {  // Yavru süreç
         close(fd[1]);
+        close(sync_pipe[1]);
 
+        char signal;
         while (1) {
+            read(sync_pipe[0], &signal, 1);  // Wait for signal from Uretici
+
             for (int i = 0; i < 10; i++) {
                 pthread_create(&threads[i], NULL, thread_fonksiyonu, NULL);
                 pthread_join(threads[i], NULL);
             }
+
+            write(sync_pipe[0], "C", 1);  // Signal to Uretici that consumption is done
         }
     }
 
--- Uretici.c
+++ Uretici.c
@@ -5,7 +5,8 @@
 
 int sayac = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-int fd[2];  // pipe için
+int fd[2];  // for data
+int sync_pipe[2];  // for synchronization
 
 void *thread_fonksiyonu(void *arg) {
     pthread_mutex_lock(&mutex);
@@ -22,7 +23,7 @@ void *thread_fonksiyonu(void *arg) {
 int main() {
     pthread_t threads[10];
 
-    if (pipe(fd) == -1) {
+    if (pipe(fd) == -1 || pipe(sync_pipe) == -1) {
         perror("pipe");
         exit(1);
     }
@@ -30,12 +31,18 @@ int main() {
     pid_t pid = fork();
     if (pid == 0) {  // Yavru süreç
         close(fd[1]);
+        close(sync_pipe[0]);
 
+        char signal;
         while (1) {
             for (int i = 0; i < 10; i++) {
                 pthread_create(&threads[i], NULL, thread_fonksiyonu, NULL);
                 pthread_join(threads[i], NULL);
             }
+            
+            write(sync_pipe[1], "P", 1);  // Signal to Tuketici that production is done
+            sayac = 0;
+            read(sync_pipe[1], &signal, 1);  // Wait for signal from Tuketici
         }
     }
 
