68ef10f3a53a859d5680b3afa982b26ad025cf8d
Mohamed Nour <alkhalafmuhamade@gmail.com>
2023-08-12 04:21:37 +0300

    a

--- Tuketici.c
+++ Tuketici.c
@@ -6,9 +6,11 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 
+int fd[2];  // pipe for communication between parent and child
+
 void *thread_fonksiyonu(void *arg) {
     int okunan_deger = *((int*) arg);
-    printf("Tüketici thread: %d okundu.\n", okunan_deger);
+    write(fd[1], &okunan_deger, sizeof(okunan_deger));  // send data to parent process
     sleep(1);
     return NULL;
 }
@@ -16,26 +18,42 @@ void *thread_fonksiyonu(void *arg) {
 int main() {
     pthread_t threads[10];
 
-    sem_t *sem_for_tuketici = sem_open("/sem_for_tuketici", O_CREAT, 0644, 0);
-    sem_t *sem_for_uretici = sem_open("/sem_for_uretici", O_CREAT, 0644, 1);
-
-    int counter = 0;
-    while (1) {
-        sem_wait(sem_for_tuketici);  // Wait for the signal from Uretici
-        for (int i = 0; i < 10; i++) {
-            counter++;
-            pthread_create(&threads[i], NULL, thread_fonksiyonu, &counter);
-            pthread_join(threads[i], NULL);
-        }
-        counter = 0;
-        sem_post(sem_for_uretici);  // Signal to Uretici to start producing
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
     }
 
-    sem_close(sem_for_tuketici);
-    sem_unlink("/sem_for_tuketici");
+    pid_t pid = fork();
+    if (pid == 0) {  // child process
+        close(fd[1]);
+        int okunan_deger;
+        while (1) {
+            read(fd[0], &okunan_deger, sizeof(okunan_deger));
+            printf("Tüketici child thread: %d okundu.\n", okunan_deger);
+        }
+    } else if (pid > 0) {  // parent process
+        close(fd[0]);
 
-    sem_close(sem_for_uretici);
-    sem_unlink("/sem_for_uretici");
+        sem_t *sem_for_tuketici = sem_open("/sem_for_tuketici", O_CREAT, 0644, 0);
+        sem_t *sem_for_uretici = sem_open("/sem_for_uretici", O_CREAT, 0644, 1);
+
+        int counter = 0;
+        while (1) {
+            sem_wait(sem_for_tuketici);  
+            for (int i = 0; i < 10; i++) {
+                counter++;
+                pthread_create(&threads[i], NULL, thread_fonksiyonu, &counter);
+                pthread_join(threads[i], NULL);
+            }
+            counter = 0;
+            sem_post(sem_for_uretici);
+        }
+
+        sem_close(sem_for_tuketici);
+        sem_unlink("/sem_for_tuketici");
+        sem_close(sem_for_uretici);
+        sem_unlink("/sem_for_uretici");
+    }
 
     return 0;
 }
--- Uretici.c
+++ Uretici.c
@@ -7,11 +7,12 @@
 
 int sayac = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int fd[2];  // pipe for communication between parent and child
 
 void *thread_fonksiyonu(void *arg) {
     pthread_mutex_lock(&mutex);
     sayac++;
-    printf("Üretici thread %d: %d yazıldı.\n", sayac, sayac);
+    write(fd[1], &sayac, sizeof(sayac));  // write data to child process
     pthread_mutex_unlock(&mutex);
     sleep(1);
     return NULL;
@@ -20,24 +21,40 @@ void *thread_fonksiyonu(void *arg) {
 int main() {
     pthread_t threads[10];
 
-    sem_t *sem_for_tuketici = sem_open("/sem_for_tuketici", O_CREAT, 0644, 0);
-    sem_t *sem_for_uretici = sem_open("/sem_for_uretici", O_CREAT, 0644, 1);  // Producer starts first
-
-    while (1) {
-        sem_wait(sem_for_uretici);  // Wait for the signal from Tuketici
-        for (int i = 0; i < 10; i++) {
-            pthread_create(&threads[i], NULL, thread_fonksiyonu, NULL);
-            pthread_join(threads[i], NULL);
-        }
-        sayac = 0;
-        sem_post(sem_for_tuketici);  // Signal to Tuketici to start consuming
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
     }
 
-    sem_close(sem_for_tuketici);
-    sem_unlink("/sem_for_tuketici");
+    pid_t pid = fork();
+    if (pid == 0) {  // child process
+        close(fd[1]);
+        int okunan_deger;
+        while (1) {
+            read(fd[0], &okunan_deger, sizeof(okunan_deger));
+            printf("Üretici child thread: %d yazıldı.\n", okunan_deger);
+        }
+    } else if (pid > 0) {  // parent process
+        close(fd[0]);
 
-    sem_close(sem_for_uretici);
-    sem_unlink("/sem_for_uretici");
+        sem_t *sem_for_tuketici = sem_open("/sem_for_tuketici", O_CREAT, 0644, 0);
+        sem_t *sem_for_uretici = sem_open("/sem_for_uretici", O_CREAT, 0644, 1);
+
+        while (1) {
+            sem_wait(sem_for_uretici);  
+            for (int i = 0; i < 10; i++) {
+                pthread_create(&threads[i], NULL, thread_fonksiyonu, NULL);
+                pthread_join(threads[i], NULL);
+            }
+            sayac = 0;
+            sem_post(sem_for_tuketici);
+        }
+
+        sem_close(sem_for_tuketici);
+        sem_unlink("/sem_for_tuketici");
+        sem_close(sem_for_uretici);
+        sem_unlink("/sem_for_uretici");
+    }
 
     return 0;
 }
